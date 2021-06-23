101da7f3f2fd871657ff43f223159c3772ed6a5e
Tomasz Fijalkowski <tomasz.fijalkowski@allegro.pl>
2021-06-23 14:27:36 +0200

    Initial commit

---
+++ src/main/kotlin/pl/tfij/notiktak/NotiktakApplication.kt
@@ -0,0 +1,11 @@
+package pl.tfij.notiktak
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotiktakApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotiktakApplication>(*args)
+}
---
+++ src/test/kotlin/pl/tfij/notiktak/NotiktakApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.tfij.notiktak
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotiktakApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
